90268d1feea94c7110c8aa3d2235b6c1aa487efe
Anandarajah Yathuvaran <yathuvaran@gmail.com>
2019-12-14 13:03:50 -0500

    updated grid

--- Tic-tac-toe/src/TictactoeView.java
+++ Tic-tac-toe/src/TictactoeView.java
@@ -1,29 +1,42 @@
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
-public class TictactoeView extends JFrame{
-	
-	TictactoeView(){
-		
+public class TictactoeView extends JFrame {
+
+	JButton[][] buttons;
+
+	TictactoeView() {
+		this.setTitle("Tic-tac-toe");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setSize(300,300);
-		JButton b1=new JButton("1");  
-	    JButton b2=new JButton("2");  
-	    JButton b3=new JButton("3");  
-	    JButton b4=new JButton("4");  
-	    JButton b5=new JButton("5");  
-	    JButton b6=new JButton("6");  
-	    JButton b7=new JButton("7");  
-	    JButton b8=new JButton("8");  
-	    JButton b9=new JButton("9");
-	    this.add(b1);this.add(b2);this.add(b3);
-	    this.add(b4);this.add(b5);this.add(b6);
-	    this.add(b7);this.add(b8);this.add(b9);
-	    
-	    this.setLayout(new GridLayout(3,3));
-	    this.setVisible(true);	
+		this.setSize(300, 300);
+		JPanel buttonPanel = new JPanel();
+		JMenuBar menuBar = new JMenuBar();
+		JMenu reset = new JMenu("Reset");
+		buttons = new JButton[3][3];
+		for (int i = 0; i < buttons.length; i++) {
+			for (int j = 0; j < buttons.length; j++) {
+				buttons[i][j] = new JButton();
+				buttons[i][j].setFont(new Font("Arial", Font.PLAIN, 40));
+				buttons[i][j].addActionListener(tictactoeListener);
+				buttonPanel.add(buttons[i][j]);
+			}
+		}
+		buttonPanel.setLayout(new GridLayout(3, 3));
+		menuBar.add(reset);
+		this.setJMenuBar(menuBar);
+		this.add(buttonPanel);
+		this.setVisible(true);
+	}
+
+	public void setButtonText(int row, int column, String text) {
+		buttons[row][column].setText(text);
+	}
+	
+	public String getButtonText(int row, int column) {
+		return buttons[row][column].getText();
 	}
 	
 	public static void main(String[] args) {
