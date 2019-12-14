695146c6b637e32374250052f059bfecad071e1f
Anandarajah Yathuvaran <yathuvaran@gmail.com>
2019-12-13 22:23:14 -0500

    added findWinner, toString, and helper method. Also created main method

--- Tic-tac-toe/src/TicTacToe.java
+++ Tic-tac-toe/src/TicTacToe.java
@@ -46,8 +46,122 @@ public class TicTacToe {
 		this.gameState = findWinner();
 		return this.gameState;
 	}
-	
-	
-	
+
+	private GameStatus findWinner() {
+		if (plays == dimension * dimension) {
+			return GameStatus.DRAW;
+		}
+		GameStatus newGameState;
+		for (int i = 0; i < dimension; i++) {
+			for (int j = 0; j < dimension; j++) {
+				if (grid[i][j] != ' ') {
+					newGameState = findWinnerFrom(i, j);
+					if (newGameState != GameStatus.IN_PROGRESS) {
+						return newGameState;
+					}
+				}
+			}
+
+		}
+		return GameStatus.IN_PROGRESS;
+	}
+
+	private GameStatus charToEnum(char player) {
+		if (player == 'X') {
+			return GameStatus.X_WON;
+		} else {
+			return GameStatus.O_WON;
+		}
+	}
+
+	private GameStatus findWinnerFrom(int row, int column) {
+
+		// Look horizontally - left than right
+
+		int count;
+
+		count = 1;
+		for (int c = column - 1; c > 0; c--) {
+			if (this.grid[row][column] == this.grid[row][c]) {
+				count++;
+				if (count == dimension) {
+					return charToEnum(grid[row][column]);
+				}
+			} // else, look in another direction
+		}
+
+		count = 1;
+		for (int c = column + 1; c < this.nColumns; c++) {
+			if (this.grid[row][column] == this.grid[row][c]) {
+				count++;
+				if (count == dimension) {
+					return charToEnum(grid[row][column]);
+				}
+			} // else, look in another direction
+		}
+
+		// Look vertically - up then down
+		count = 1;
+		for (int r = row - 1; r > 0; r--) {
+			if (this.grid[r][column] == this.grid[row][column]) {
+				count++;
+				if (count == dimension) {
+					return charToEnum(grid[row][column]);
+				}
+			} // else, look in another direction
+		}
+
+		count = 1;
+		for (int r = row + 1; r < dimension; r++) {
+			if (this.grid[row][column] == this.grid[r][column]) {
+				count++;
+				if (count == dimension) {
+					return charToEnum(grid[row][column]);
+				}
+			} // else, look in another direction
+		}
+
+		// Look diagonally - Left-down - TBD
+
+		// Look diagonally - Right-down - TBD
+
+		return GameStatus.IN_PROGRESS;
+	}
+
+	public int getTurn() {
+		return this.turn;
+	}
+
+	public GameStatus getGameState() {
+		return this.gameState;
+	}
+
+	public String toString() {
+		String s = "";
+		for (int r = 0; r < dimension; r++) {
+			for (int c = 0; c < dimension; c++) {
+				s += grid[r][c] + " | ";
+			}
+			s += "\n";
+		}
+		return s;
+	}
+
+	public static void main(String args[]) {
+		TicTacToe game = new TicTacToe('X');
+		Scanner scanner = new Scanner(System.in);
+
+		do {
+			System.out.println(game.toString());
+			System.out.println(game.getTurn() + ": Where do you want to mark? Enter row column");
+			int row = scanner.nextInt();
+			int column = scanner.nextInt();
+			scanner.nextLine();
+			game.takeTurn(row, column);
+
+		} while (game.getGameState() == TicTacToeEnum.IN_PROGRESS);
+		System.out.println(game.getGameState());
+
+	}
 
 }
